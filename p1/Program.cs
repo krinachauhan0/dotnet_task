e865817183b0429bf9cea031f88ead2c2a5500c2
krinachauhan0 <krina.c@masterlysolutions.com>
2023-01-17 19:26:57 +0530

    -- .net task

---
+++ p1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwappingVariable
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            //using third variable
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("After swapping a : " + a);
+            Console.WriteLine("After swapping b : " +b);
+        }
+    }
+}
---
+++ p1a/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwappingVariable
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            //without using third variable
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("After swapping a : " + a);
+            Console.WriteLine("After swapping b : " +b);
+        }
+    }
+}
